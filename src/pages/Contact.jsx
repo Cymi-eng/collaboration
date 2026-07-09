ebcc8ad0bf38e28ff753092ec52dc07b423d5bab
Idahk19 <idahkarwitha9@gmail.com>
2026-07-09 10:43:14 +0300

    Create a contact page with contact details

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>collaboration@gmail.com</p>
+      <p>0757732215</p>
+    </div>
+  )
+}
+
+export default Contact
